4631f72b81696078d64ce179b01b54e91f4a5c5c
liulei <ll4285@126.com>
2020-07-24 20:50:09 +0800

    commit tabbar navbar and swiper

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,44 @@
 <template>
-    <h2>首页</h2>
+  <div>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+
+  import NavBar from "components/common/navbar/NavBar";
+  import { getHomeMultidata } from 'network/home';
+  import HomeSwiper from "./HomeSwiper";
+  import RecommendView from "./RecommendView";
+
+
+  export default {
+    name: "Home",
+    data(){
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    components: {
+      RecommendView,
+      NavBar,
+      HomeSwiper,
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
     }
+  }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-text);
+    color: #FFFFFF;
+  }
 </style>
